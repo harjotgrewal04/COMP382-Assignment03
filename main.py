5bc2c6d47f12f801b4c6109bd0b45e27a65cadfc
Gurv-Chahal <gurveer.chahal15@icloud.com>
2026-04-03 14:38:11 -0700

    working on part 5, starting by hardcoding some test clauses until we finish part 3 and 4

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ if __name__ == "__main__":
     clauses = parse_3sat_formula(formula_text, expected_k=8)
 
     print("Parsed clauses:" , clauses)
+
+    # hardcoding test for part 5 until part 3 and 4 are done
+    test_clauses = [
+        ["x1", "x2", "x3"],
+        ["~x1", "x2", "x4"],
+        ["x1", "~x2", "x5"],
+        ["x2", "x3", "~x4"],
+        ["~x3", "x4", "x6"],
+        ["x1", "~x5", "x6"],
+        ["~x2", "x5", "x6"],
+        ["x3", "~x4", "~x6"]
+    ]
+
+    test_assignment = {
+        "x1": True, "x2": True, "x3": True,
+        "x4": False, "x5": True, "x6": True,
+    }
     
 
 
